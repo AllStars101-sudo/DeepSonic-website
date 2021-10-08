619e8e137f24a1a4a97dd8b791da7043a1864295
Chris Pagolu <53670363+AllStars101-sudo@users.noreply.github.com>
2021-10-08 16:02:25 +0800

    ok

--- DeepSonic/main.py
+++ DeepSonic/main.py
@@ -453,6 +453,4 @@ VANTA.NET({
 
 
 if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', debug=True)
+    app.run(debug=False)
